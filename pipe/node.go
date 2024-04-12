d7b795a4dd4fa28b13c05799ecd2b53b86a74517
Mario Macias <mario.macias@grafana.com>
2024-04-12 16:48:50 +0200

    removed documents

--- pipe/node.go
+++ pipe/node.go
@@ -1,8 +1,6 @@
 // Package node provides functionalities to create nodes and interconnect them.
 // A Node is a function container that can be connected via channels to other nodes.
 // A node can send data to multiple nodes, and receive data from multiple nodes.
-//
-//nolint:unused
 package pipe
 
 import (
