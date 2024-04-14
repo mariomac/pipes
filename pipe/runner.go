4a908ff583a79d02ca62603514942c5dbcf54014
mariomac <mmaciasl@redhat.com>
2024-04-14 17:50:15 +0200

    first version of the documentation

--- pipe/runner.go
+++ pipe/runner.go
@@ -1,16 +1,25 @@
 package pipe
 
+// Runner stores all the configured nodes of a pipeline once their nodes
+// are instantiated (as specified by AddStart, AddStartProvider,
+// AddMiddle, AddMiddleProvider, AddFinal, AddFinalProvider) and connected
+// according to the Connect function of the NodesMap that is provided to
+// the Builder.
 type Runner struct {
 	startNodes []startable
 	finalNodes []doneable
 }
 
+// Start the pipeline processing in a background.
 func (b *Runner) Start() {
 	for _, s := range b.startNodes {
 		s.Start()
 	}
 }
 
+// Done returns a channel that is closed when all the nodes of the
+// pipeline have stopped processing data. This is, the functions running
+// the node logic have returned.
 func (b *Runner) Done() <-chan struct{} {
 	done := make(chan struct{})
 	go func() {
